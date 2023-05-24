a666371cd4dff210a121ac665b8a0791e7a398b3
Ochan-LOKIDORMOI <116851021+Ochan-LOKIDORMOI@users.noreply.github.com>
2023-05-24 13:20:26 +0200

    Update hex_color_code.py

--- hex_color_code.py
+++ hex_color_code.py
@@ -2,9 +2,18 @@
 
 import re
 
-    string = "Games Of Throne (2011), [Verse 1] I can't hide myself I don't expect you to understand I just hope I can explain What it's like to be a man It's a lonely road And they don't care 'bout what you know It's not 'bout how you feel But what you provide inside that home, @DenmarkEddy, ISBN 1234-5-678-90123-4, Why did the joke? Because..., Games Of Throne S01E01: Episode Title, 01-Jan-2023, color: #ABC123, 192.168.0.1"
-    pattern = r"Color: (#\w{6})"
+string = """
+Movie: "Game of thrones (2011), House of dragons (2022),
+Song: [Verse 1] I can't hide myself I don't expect you understand I just hope I can explain What it's like to be a man It's a lonely road,
+Twitter: @DenmarkEddy
+ISBN: ISBN 123-4-567-89012-3
+Joke: Why did the chicken cross the road? Because it wanted to get to the other side, Why did the seagulls fly over the ocean? Because if they flew over the bay
+TV Episode: Show Name S01E02: Episode Title
+Date: 24-May-2023
+Color: #FF0000
+IP Address: 192.168.0.1
+"""
+pattern = r"Color: (#\w{6})"
 
-    result = re.findall(pattern, string)
-
-    print(result)
+result = re.findall(pattern, string)
+print(result)
